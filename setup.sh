2018238c906e47918ea23ab4db71d052122f650a
jex-lin <italwaysrainonme@gmail.com>
2014-01-26 02:06:23 +0800

    Add go.snippets

--- setup.sh
+++ setup.sh
@@ -24,15 +24,16 @@ fi
 # Create symbolic link 
 echo "=== Create symbolic link ==="
 ln -s vim .vim
-ln -s .vim/vimrc .vimrc
+ln -s vim/vimrc .vimrc
 
 # Download vundle and other plugins
 echo "=== Download vundle and other plugins ==="
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+git clone https://github.com/gmarik/vundle.git ~/vim/bundle/vundle
 vim +BundleInstall +qall
 
 # Combine snippets
 echo "=== Combine snippets === "
-cat .vim/additional-setting/snippets/php.snippets >> .vim/bundle/snipmate.vim/snippets/php.snippets
+cat vim/additional-setting/snippets/php.snippets >> vim/bundle/snipmate.vim/snippets/php.snippets
+cp vim/additional-setting/snippets/go.snippets vim/bundle/snipmate.vim/snippets/
 
 echo "=== Setup done !! ==="
