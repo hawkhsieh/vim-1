607eba19d44bd081df7365957ff18f0a8ca44767
Jex Lin <jex_lin@miiicasa.com>
2014-10-02 15:17:21 +0800

    Modify php.snippets and setup mv->cp

--- setup.sh
+++ setup.sh
@@ -37,8 +37,8 @@ echo "=== Combine snippets === "
 # cat vim/additional-setting/snippets/go.snippets >> vim/bundle/snipmate.vim/snippets/go.snippets
 # cat vim/additional-setting/snippets/javascript.snippets >> vim/bundle/snipmate.vim/snippets/javascript.snippets
 
-mv vim/additional-setting/snippets/php.snippets vim/bundle/snipmate.vim/snippets/php.snippets
-mv vim/additional-setting/snippets/go.snippets vim/bundle/snipmate.vim/snippets/go.snippets
-mv vim/additional-setting/snippets/javascript.snippets vim/bundle/snipmate.vim/snippets/javascript.snippets
+cp vim/additional-setting/snippets/php.snippets vim/bundle/snipmate.vim/snippets/php.snippets
+cp vim/additional-setting/snippets/go.snippets vim/bundle/snipmate.vim/snippets/go.snippets
+cp vim/additional-setting/snippets/javascript.snippets vim/bundle/snipmate.vim/snippets/javascript.snippets
 
 echo "=== Setup done !! ==="
